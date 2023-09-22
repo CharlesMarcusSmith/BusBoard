f582be893458fe9694f2756db69ae91677115e02
CharlesMarcusSmith <charliemarcussmith@googlemail.com>
2023-09-22 17:30:31 +0100

    CS: Stops Strop results stored and sorted.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.*;
+
 import org.json.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -19,10 +21,15 @@ public class Main {
         // Build HTTP client, whatever that means --/
         //Convert JSON String result to JSON Objects
         //Create a stopInfo or stopTime Class for storing info.
+        //Sort stop list
+        // ^ require time to arrival as int.
         // Print out next 5 busses at selected stop
         // May need Query parameter at the end for app key or something?
 
         // Ask user for Post Code and take Stop Code as Input
+        //"Get list of bus stops in a specified...
+        //^ take lat, long, bus stop type V, and radius
+        //NaptanOnstreetBusCoachStopPair ??
 
         String stopCode = "";
         UserInputScanner uis = new UserInputScanner();
@@ -54,18 +61,42 @@ public class Main {
         // { … } represents an object, so library will parse it to JSONObject
 
         JSONArray jsonArray = new JSONArray(jsonString); //inside [ means its array, so this turns string to array
+        List<StopInfo> stops = new ArrayList<>();
 
-        for(int i=0; i<jsonArray.length(); i++){
+        for(int i=0; i<jsonArray.length(); i++) {
             JSONObject jObject = jsonArray.getJSONObject(i); //looping through array and getting JSONobject { one at a time
-            System.out.println(jObject.get("expectedArrival"));
 
             String id = jObject.get("id").toString();
-            System.out.println(id);
+            String stationName = jObject.get("stationName").toString();
+
+            String expectedArrival = jObject.get("expectedArrival").toString();
+            //Countdown:
+            String timestamp = jObject.get("timestamp").toString();
+            String tts = jObject.get("timeToStation").toString();
+            int timeToStation = Integer.parseInt(tts);
+
+            StopInfo si = new StopInfo(id, stationName, expectedArrival, timestamp, timeToStation);
+            stops.add(si);
         }
 
-
-
-
-
+        try {
+            System.out.println("Trying to sort");
+            Collections.sort(stops, new Comparator<StopInfo>() {
+                @Override
+                public int compare(StopInfo o1, StopInfo o2) {
+                    int comp = o1.getTimeToStation() - o2.getTimeToStation();
+                    return comp;
+                }
+            });
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("List:");
+        for(int i=0; i<stops.size(); i++) {
+            StopInfo tempStopInfo = stops.get(i);
+            System.out.println(tempStopInfo.getTimeToStation());
+        }
     }
+
+
 }
--- src/main/java/org/example/StopInfo.java
+++ src/main/java/org/example/StopInfo.java
@@ -1,5 +1,39 @@
 package org.example;
 
 public class StopInfo {
+    String id;
+    String stationName;
+    String expectedArrival;
 
+    //Countdown:
+    String timestamp;
+    int timeToStation;
+
+    public StopInfo(String id, String stationName, String expectedArrival, String timestamp, int timeToStation) {
+        this.id = id;
+        this.stationName = stationName;
+        this.expectedArrival = expectedArrival;
+        this.timestamp = timestamp;
+        this.timeToStation = timeToStation;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public String getExpectedArrival() {
+        return expectedArrival;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public int getTimeToStation() {
+        return timeToStation;
+    }
 }
