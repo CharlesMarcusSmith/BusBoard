26af678f89a428c94f9341279256246ce62cbd78
CharlesMarcusSmith <charliemarcussmith@googlemail.com>
2023-09-24 08:01:51 +0100

    CS: Solved the 'scaner.nextLine(); finished with non-zero exit value 1' bug, moved scanner decleration outside of methods and did not close them. Not best practice but it runs, will try to remember at the end of the input usage to close.

--- src/main/java/org/example/UserInputScanner.java
+++ src/main/java/org/example/UserInputScanner.java
@@ -3,14 +3,15 @@ package org.example;
 import java.util.Scanner;
 
 public class UserInputScanner {
+    Scanner sc = new Scanner(System.in);
+
     public String busStopCodeUserInput(){
         String stopCode = "";
-        Scanner sc = new Scanner(System.in);
 
         System.out.println("Please enter the bus stop code you wish to search");
         System.out.println("For Example: 490000129R");
         stopCode = sc.nextLine();
-        sc.close();
+
         return stopCode;
     }
 
